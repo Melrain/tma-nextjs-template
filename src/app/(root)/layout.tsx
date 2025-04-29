df6d88eda1f780057ea3599bc4118315859b08b3
Melrain <yezyd92@icloud.com>
2025-04-30 00:57:34 +0800

    ws

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -15,7 +15,7 @@ const layout = ({
   const _initData = parseInitData(initData.raw());
   const tonWalletAddress = useTonAddress();
   const params = {
-    url: "http://3.80.125.152:8080/",
+    url: "ws://3.80.125.152:8080",
     userId: _initData.user?.id || "0",
     username: _initData.user?.firstName || "user",
     initDataRaw: initData.raw(),
